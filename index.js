0ef677501330c18e8efce7a58c6d84786dd0620b
JubayerDev <ajashes858@gmail.com>
2022-12-04 16:54:50 +0600

    course and single course loaded

--- index.js
+++ index.js
@@ -5,9 +5,26 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const categories = require('./data/categories.json');
+const courses = require('./data/courses.json');
+
 
 app.get('/', (req, res) => {
     res.send('GoStudy is Running');
+})
+
+app.get('/categories', (req, res) => {
+    res.send(categories)
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+
+app.get('/courses/:id', (req, res)=>{
+    const id = parseInt(req.params.id);
+    const singleCourse = courses.find(course => course.id === id);
+    res.send(singleCourse);
 })
 
 app.listen(port, () => {
