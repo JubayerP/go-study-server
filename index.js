dcd514b82400c033aaca655f3878aed188affb6f
JubayerDev <ajashes858@gmail.com>
2022-12-05 14:36:21 +0600

    comments added

--- index.js
+++ index.js
@@ -14,24 +14,29 @@ app.get('/', (req, res) => {
     res.send('GoStudy is Running');
 })
 
+// Categories
 app.get('/categories', (req, res) => {
     res.send(categories)
 })
 
+// Courses
 app.get('/courses', (req, res) => {
     res.send(courses)
 })
 
+// Single Courses
 app.get('/courses/:id', (req, res)=>{
     const id = parseInt(req.params.id);
     const singleCourse = courses.find(course => course.id === id);
     res.send(singleCourse);
 })
 
+// Skills
 app.get('/skills', (req, res) => {
     res.send(skills)
 })
 
+// Port Connects
 app.listen(port, () => {
     console.log(`Go study is serving on ${port}`);
 })
